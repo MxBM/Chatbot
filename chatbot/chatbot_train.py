c0b09b1ce70be5af45941f9bdefe25c927680d4c
officialsmp305 <Officialsmp305@gmail.com>
2022-04-17 17:07:21 -0700

    [UPDATE]: Seq2Seq Model Training W/ Checkpoints

--- chatbot/chatbot_train.py
+++ chatbot/chatbot_train.py
@@ -11,7 +11,7 @@ import random
 import os
 import itertools
 
-from text_processing import SOS_FLAG, EOS_FLAG, PAD_FLAG, MAX_LEN, voc, corpus_name, pairs, save_dir
+from text_processing import SOS_FLAG, EOS_FLAG, PAD_FLAG, MAX_LEN, voc, corpus_name, pairs, save_dir, normalizeString
 from torch import optim
 
 CUDA = torch.cuda.is_available()
@@ -348,22 +348,23 @@ attention_model = 'dot'
 encoder_n_layers = 2
 decoder_n_layers = 2
 dropoutRate = 0.1
-batch_size = 64  # NOTE: MAKE THIS SMALLER IF YOU DON'T HAVE A GPU
+batch_size = 100  # NOTE: MAKE THIS SMALLER IF YOU DON'T HAVE A GPU
 train_loss = []
 clip = 50.0
 teacher_forcing_ratio = 1.0
 learning_rate = 0.0001
 decoder_learning_ratio = 5.0
-n_iteration = 500
+n_iteration = 5000
 print_every = 10
 save_every = 250
 
 # Set the checkpoint to load from; set to None if you're training the model from scratch
-loadFileName = None
-#checkpoint_iter = 200000
-# loadFilename = os.path.join(save_dir, model_name, corpus_name,
-#                           '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size),
-#                             '{}_checkpoint.tar'.format(checkpoint_iter))
+#loadFileName = None
+checkpoint_iter = 250
+loadFileName = os.path.join(save_dir, model_name, corpus_name,
+                            '{}-{}_{}'.format(encoder_n_layers,
+                                              decoder_n_layers, hidden_size),
+                            '{}_checkpoint.tar'.format(checkpoint_iter))
 
 if loadFileName:  # Check if you're loading from the machine the model was trained on
     checkpoint = torch.load(loadFileName)
@@ -405,3 +406,111 @@ print("Starting Training!")
 trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer,
            embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size,
            print_every, save_every, clip, corpus_name, loadFileName)
+
+
+class GreedySearchDecoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(GreedySearchDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, input_seq, input_length, max_length):
+        # Forward input through encoder model
+        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+        # Prepare encoder's final hidden layer to be first hidden input to the decoder
+        decoder_hidden = encoder_hidden[:decoder.n_layers]
+        # Initialize decoder input with SOS_token
+        decoder_input = torch.ones(
+            1, 1, device=device, dtype=torch.long) * SOS_FLAG
+        # Initialize tensors to append decoded words to
+        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
+        all_scores = torch.zeros([0], device=device)
+        # Iteratively decode one word token at a time
+        if not USE_MULTINOMIAL:
+            for _ in range(max_length):
+                # Forward pass through decoder
+                decoder_output, decoder_hidden = self.decoder(
+                    decoder_input, decoder_hidden, encoder_outputs)
+                # Obtain most likely word token and its softmax score
+                decoder_scores, decoder_input = torch.max(
+                    decoder_output, dim=1)
+                # Record token and score
+                all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+                all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+                # Prepare current token to be next decoder input (add a dimension)
+                decoder_input = torch.unsqueeze(decoder_input, 0)
+            # Return collections of word tokens and scores
+            return all_tokens, all_scores
+        else:
+            for _ in range(max_length):
+                # Forward pass through decoder
+                decoder_output, decoder_hidden = self.decoder(
+                    decoder_input, decoder_hidden, encoder_outputs)
+
+                # Sample from the network as a multinomial distribution
+                decoder_output_multi = decoder_output.data.view(
+                    -1).div(TEMP).exp()
+                decoder_input = torch.multinomial(decoder_output_multi, 1)
+                decoder_scores, _ = torch.max(decoder_output, dim=1)
+                # Record token and score
+                all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+                all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+                # Prepare current token to be next decoder input (add a dimension)
+                decoder_input = torch.unsqueeze(decoder_input, 0)
+            # Return collections of word tokens and scores
+            return all_tokens, all_scores
+
+
+def evaluate(encoder, decoder, searcher, voc, sentence, max_length=MAX_LEN, temperature=False):
+    # Format input sentence as a batch
+    # words -> indexes
+    indexes_batch = [indexesFromSentence(voc, sentence)]
+    # Create lengths tensor
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    # Transpose dimensions of batch to match models' expectations
+    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+    # Use appropriate device
+    input_batch = input_batch.to(device)
+    lengths = lengths.to(device)
+    # Decode sentence with searcher
+    tokens, scores = searcher(input_batch, lengths, max_length)
+    # indexes -> words
+    decoded_words = [voc.index2word[token.item()] for token in tokens]
+    return decoded_words
+
+
+def evaluateInput(encoder, decoder, searcher, voc):
+    input_sentence = ''
+    while(1):
+        try:
+            # Get input sentence
+            input_sentence = input('> ')
+            # Check if it is quit case
+            if input_sentence == 'q' or input_sentence == 'quit':
+                break
+            # Normalize sentence
+            input_sentence = normalizeString(input_sentence)
+            # Evaluate sentence
+            output_words = evaluate(
+                encoder, decoder, searcher, voc, input_sentence)
+            # Format and print response sentence
+            output_words[:] = [x for x in output_words if not (
+                x == 'EOS' or x == 'PAD')]
+            print('Bot:', ' '.join(output_words))
+
+        except KeyError:
+            print("Error: Encountered unknown word.")
+
+
+USE_MULTINOMIAL = False
+TEMP = 0.7
+
+# Set dropout layers to eval mode
+encoder.eval()
+decoder.eval()
+
+# Initialize search module
+searcher = GreedySearchDecoder(encoder, decoder)
+
+# Begin chatting (uncomment and run the following line to begin)
+evaluateInput(encoder, decoder, searcher, voc)
