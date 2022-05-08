9ecf71e1346b8d4ad5fdc3e6a685608cdf4a10bd
officialsmp305 <Officialsmp305@gmail.com>
2022-05-07 21:49:47 -0700

    [TEMP] Format Reddit Data Into Single File (Error)

--- chatbot/text_processing_reddit_data.py
+++ chatbot/text_processing_reddit_data.py
@@ -1,47 +1,32 @@
-import sqlite3
-import pandas as pd
+import os
+import codecs
+import re
+import csv
 
-timeframes = ["2017-11"]
+"""
+This file is dedicate to preprocessing the raw data reddit into a text file full of sequential conversations.
+"""
 
-for timeframe in timeframes:
-    connection = sqlite3.connect("{}.db".format(timeframe))  # datbase file
-    c = connection.cursor()
-    limit = 5000  # Limit for the pull
-    last_unix = 0  # Buffer in each pull
-    cur_length = limit
-    counter = 0
-    test_done = False
+datasetName = "custom reddit-dialogs dataset"
+dataset = os.path.join("chatbot\data", datasetName)
 
-    while (
-        cur_length == limit
-    ):  # Pull exhausted the limit as zero rows of data were left
-        df = pd.read_sql(
-            "SELECT * FROM parent_reply WHERE unix > {} and parent NOT NULL and score > 0 ORDER BY unix ASC LIMIT {}".format(
-                last_unix, limit
-            ),
-            connection,
-        )
-        last_unix = df.tail(1)["unix"].values[0]
-        cur_length = len(df)
-        if not test_done:
-            with open("test.from", "a", encoding="utf8") as f:
-                for content in df["parent"].values:
-                    f.write(content + "\n")
-            with open("test.to", "a", encoding="utf8") as f:
-                for content in df["comment"].values:
-                    f.write(content + "\n")
-            # TODO: Update cur_length and limit here
-            test_done = True
-        else:  # Assuming test is complete
-            with open(
-                "train.from", "a", encoding="utf8"
-            ) as f:  # TODO: Turn this into a function
-                for content in df["parent"].values:
-                    f.write(content + "\n")
-            with open("train.to", "a", encoding="utf8") as f:
-                for content in df["comment"].values:
-                    f.write(content + "\n")
-        counter += 1
+parentData = os.path.join(dataset, "reddit_parent_reply.txt")
+childData = os.path.join(dataset, "reddit_child_reply.txt")
 
-        if counter % 20 == 0:
-            print(counter * limit, "rows completed so far")
+question = []
+reply = []
+
+with open(parentData, encoding='iso-8859-1') as parent:
+    with open(childData, encoding='iso-8859-1') as child:
+        with open("chatbot/test.txt", "w", encoding="utf-8") as newFile:
+            question = parent.readlines()
+            reply = child.readlines()
+
+            for line in question, reply:
+                line = re.sub(
+                    r'\s*(?:https?://)?www\.\S*\.[A-Za-z]{2,5}\s*', "", str(line))
+                line = re.sub("newlinechar", "", str(line))
+
+            for i in range(len(question)):
+                line = question[i].strip() + "\n" + reply[i]
+                newFile.write(line)
